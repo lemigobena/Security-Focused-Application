01fd97f171f4fc06b7b6e33fbb638364b5cd9c6c
lemigobena <lemigobena12@gmail.com>
2026-05-13 17:01:37 +0300

    chore: update CORS configuration to allow all origins via wildcard boolean

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -24,11 +24,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(
   cors({
-    origin: [
-      'https://security-focused-application-rb3m-lbn230thf.vercel.app',
-      'https://security-focused-application-rb3m.vercel.app',
-      'http://localhost:5173'
-    ],
+    origin: true,
     credentials: true, // Allow cookies to be sent
   })
 );
